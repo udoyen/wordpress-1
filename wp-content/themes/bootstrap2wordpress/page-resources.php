9e819b003bf8a5ce6018a7db21f1c1bb9cbc3a02
udoyen <datameshprojects@gmail.com>
2016-02-25 10:58:03 +0100

    resources page done

--- wp-content/themes/bootstrap2wordpress/page-resources.php
+++ wp-content/themes/bootstrap2wordpress/page-resources.php
@@ -5,22 +5,26 @@
 
 get_header();
 
-$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID));
-		
+$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
+
+//$resource_image = get_field( 'resource_image' );
+//$resource_url = get_field( 'resource_url' );
+//$add_button = get_field( 'add_button' );
+//$button_text = get_field( 'button_text' );
 ?>
 
 <!-- FEATURE IMAGE ========================================= -->
-<?php if(  has_post_thumbnail() ) { // check for feature image ?>
+<?php if ( has_post_thumbnail() ) { // check for feature image   ?>
 
-<section class="feature-image" style="background: url('<?php echo $thumbnail_url; ?>') no-repeat; background-size: cover;" data-type="background" data-speed="2">
-	<h1 class="page-title"><?php echo the_title(); ?></h1>
-</section>
+	<section class="feature-image" style="background: url('<?php echo $thumbnail_url; ?>') no-repeat; background-size: cover;" data-type="background" data-speed="2">
+		<h1 class="page-title"><?php echo the_title(); ?></h1>
+	</section>
 
-<?php } else { // fallback image ?> 
+<?php } else { // fallback image   ?> 
 
-<section class="feature-image feature-image-default" data-type="background" data-speed="2">
-	<h1 class="page-title"><?php echo the_title(); ?></h1>
-</section>
+	<section class="feature-image feature-image-default" data-type="background" data-speed="2">
+		<h1 class="page-title"><?php echo the_title(); ?></h1>
+	</section>
 
 <?php } ?>
 
@@ -29,71 +33,39 @@ $thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id($post->ID));
 	<div class="row" id="primary">
 		<div id="content" class="col-sm-12">
 			<section class="main-content">
-				<p class="lead">Welcome to the Boostrap to Wordpress resources page! On this page, I will provide you
-					with useful resources, plugins, tools, applications, and websites to help make your life as a coder
-					a little bit easier. <strong>I recommend adding this page to your bookmarks for convenient future
-					reference</strong></p>
-				<p><em>Please keep in mind that some of the following resources are my affiliate links,
-				and should you decide to make a purchase, I will earn a small commision &mdash; at no extra cost to you!
-				I am recommending these resources only because I have found them to be incredibly helpful.
-				Don&rsquo;t spend your money on my products or resources unless you think they will benefit you.</em></p>
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php the_content(); ?>
+				<?php endwhile; // end while loop ?>
+
+				<?php $loop = new WP_Query( array( 'post_type' => 'resource', 'orderby' => 'post_id', 'order' => 'ASC' ) ); ?>
+
+				<hr>
+
+				<div class="resource-row clearfix">
+
+					<?php while ( $loop->have_posts() ): $loop->the_post(); ?>
+						<?php
+							$resource_image	= get_field('resource_image');
+							$resource_url	= get_field('resource_url');
+							$button_text	= get_field('button_text');	
+						?>
+
+						<div class="resource">
+							<img src="<?php echo $resource_image['url']; ?>" alt="<?php echo $resource_image['alt']; ?>">
+							<h3><a href="<?php echo $resource_url; ?>"><?php echo the_title(); ?></a></h3>
+							<?php echo the_content(); ?>
+
+							<?php if ( !empty( $button_text ) ) : ?>
+
+								<a href="<?php echo $resource_url; ?>" class="btn btn-success"><?php echo $button_text; ?></a>
+							<?php endif; ?>
+						</div><!-- resource -->
+
+					<?php endwhile; ?>
+
+				</div><!-- resource-row -->
 			</section><!-- main-content -->
-			<hr>
-
-			<div class="resource-row clearfix">
-				<div class="resource">
-					<img src="assets/img/justhost-logo.jpg" alt="Justhost">
-					<h3><a href="http://bradhussey.ca/justhost">JustHost</a></h3>
-					<p>When I made my very first website about 7 years ago,
-					I signed up with Justhost  and Ive never looked back!
-					Since then, Ive built hundreds of websites with Justhost as my only hosting provider.
-					I recommend these guys to all of my clients, my students, my family and friends.</p>
-
-					<p>If you&rsquo;re looking to start your very first website, or are looking for a reliable
-					hosting provider  look no further. You get a free domain name for life,
-					great customer service, unlimited domains, unlimited email accounts,
-					unlimited GBs of space, an anytime money back guarantee
-					and my favourite part: You can set up a blog website in minutes
-					with 1-click WordPress Install.
-					These guys are awesome!</p>
-
-					<a href="http://bradhussey.ca/justhost" class="btn btn-success">Get started with Justhost</a>
-
-				</div><!-- resource -->
-
-				<div class="resource">
-					<img src="assets/img/coda2-logo.jpg" alt="Coda 2">
-					<h3><a href="http://bradhussey.ca/justhost">JustHost</a></h3>
-					<p>If you&rsquo;re serious about coding, or you want to start out with a solid tool to code with,
-						I strongly recommend Coda 2 for Mac. A beautiful User Interface with tabbed navigation,
-						a super sidebar, and a simple layout. The features are outrageous and extensive,
-						with code folding, smart complete, auto indentation, GIT integration, a built-in terminal,
-						iCloud support  to name a few.
-						The features seem to be endless.</p>
-
-					<p>I use Coda 2 for all of my courses &amp; tutorials, and if you decide to use Coda 2 as your editor of choice,
-						it would greatly benefit you when taking my courses.
-						The price of the editor weighs in at $75, but its worth every penny  in my honest opinion.</p>
-
-					<a href="http://bradhussey.ca/coda2" class="btn btn-success">Check out Coda 2</a>
-
-				</div><!-- resource -->
-
-				<div class="resource">
-					<img src="assets/img/dropbox-logo.jpg" alt="Dropbox">
-					<h3><a href="http://bradhussey.ca/justhost">Dropbox</a></h3>
-					<p>This tool should go without saying. But if I have to explain  you need this tool. I use it 24/7  literally.
-						Dropbox is where I store my personal & business related data.
-						Its accessible anywhere, and regularly backs up my important information.</p>
-
-					<p>Because my data is accessible anywhere, Dropbox allows me to work on a client project at home, on the bus,
-						in a coffee shop, or at a friends house.
-						Its truly a gem of a tool, and I think you&rsquo;ll believe me.</p>
-
-					<a href="http://bradhussey.ca/getdropbox" class="btn btn-success">Try Dropbox for free</a>
-
-				</div><!-- resource -->
-			</div><!-- resource-row -->
 		</div><!-- content -->
 
 	</div><!-- row -->
