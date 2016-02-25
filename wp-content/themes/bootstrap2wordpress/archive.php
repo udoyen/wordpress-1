3ecbdc164e0bd6d97700c141cdf0acd72843f1e0
udoyen <datameshprojects@gmail.com>
2016-02-25 20:18:50 +0100

    archive template done

--- wp-content/themes/bootstrap2wordpress/archive.php
+++ wp-content/themes/bootstrap2wordpress/archive.php
@@ -6,46 +6,59 @@
  *
  * @package Boostrap_to_Wordpress
  */
+get_header();
+?>
 
-get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+
+<?php if ( have_posts() ) : ?>
+
+
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
 
 		<?php
-		if ( have_posts() ) : ?>
+		the_archive_title( '<h1 class="page-title"> ', '</h1>' );
+		the_archive_description( '<small class="taxonomy-description">', '</small>' );
+		?>
 
-			<header class="page-header">
+
+
+
+	</section>
+	<div class="container">
+		<div class="row" id="primary">
+			<main id="content" class="col-sm-8">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() );
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				endwhile;
 
-			endwhile;
+				the_posts_navigation();
 
-			the_posts_navigation();
+			else :
 
-		else :
+				get_template_part( 'template-parts/content', 'none' );
 
-			get_template_part( 'template-parts/content', 'none' );
+			endif;
+			?>
 
-		endif; ?>
-
-		</main><!-- #main -->
+		</main><!-- #content -->
+		<!-- SIDEBAR ========================== -->
+		<aside class="col-sm-4">
+			<?php get_sidebar(); ?>
+		</aside>
 	</div><!-- #primary -->
+</div><!-- .container -->
+
+
 
 <?php
-get_sidebar();
 get_footer();
