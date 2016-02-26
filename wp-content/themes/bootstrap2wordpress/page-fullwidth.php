31138c5cfb26e6b4f7da0e657b97a0ce3757792c
udoyen <datameshprojects@gmail.com>
2016-02-26 12:09:01 +0100

    full width template done

--- wp-content/themes/bootstrap2wordpress/page-fullwidth.php
+++ wp-content/themes/bootstrap2wordpress/page-fullwidth.php
@@ -1,8 +1,41 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*
+ * Template Name: Full-Width Template
  */
+$thumbnail_url = wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) );
+get_header();
+?>
 
+<!-- FEATURE IMAGE
+================================================== -->
+
+<?php if ( has_post_thumbnail() ) { ?>
+	<section class="feature-image feature-image-default" style="background:url('<?php $thumbnail_url; ?>') no-repeat; background-size:cover" data-type="background" data-speed="2">
+		<h1 class='page-title'><?php the_title(); ?></h1>
+	</section>
+
+<?php } else { // fallback image ?>
+	<section class="feature-image feature-image-default" data-type="background" data-speed="2">
+		<h1 class='page-title'><?php the_title(); ?></h1>
+	</section>
+<?php } ?>
+<div class="container">
+	<div class="row" id="primary">
+
+		<div id="content" class="col-sm-12">
+
+			<section class="main-content">
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php the_content(); ?>
+				<?php endwhile; // end the loop ?>
+			</section><!-- main-content -->
+
+		</div><!-- content -->
+
+	</div><!-- primary -->
+</div><!-- container -->
+
+
+
+<?php
+get_footer();
